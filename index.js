7516d5a58a131f4feed3eb6cb0c360f900cb3e55
Joseph Dykstra <josephdykstra@gmail.com>
2014-08-28 16:56:15 -0500

    v1.1.0 Added debouncing

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 var nodemailer = require("nodemailer")
 var xtend = require('xtend')
 
-module.exports = function LoginMailer(emitter, createHtmlEmail, transportOptions, defaultMailOptions, callback) {
+module.exports = function loginMailer(emitter, createHtmlEmail, transportOptions, defaultMailOptions, callback) {
 	var transport = nodemailer.createTransport(transportOptions)
 
-	emitter.on('authentication initiated', function(loginRequest) {
-		transport.sendMail(xtend({
-			to: loginRequest.contactAddress,
-			html: createHtmlEmail(loginRequest.token)
-		}, defaultMailOptions), callback)
+	var allowEmailToBeSent = true
+	var delayTimes = [0, 5, 10, 15, 30, 180] //seconds
+	var delayTimeIndex = 0
+	var lockTime = process.uptime()
+
+	emitter.on('authentication initiated', function (loginRequest) { //don't use timers plz. they make tests hang.
+		if (process.uptime() >= lockTime + delayTimes[delayTimeIndex]) {
+			//console.log('pass -> now:'+process.uptime()+', waitUntil:'+(lockTime+delayTimes[delayTimeIndex]))
+			if (delayTimeIndex < delayTimes.length-1) {
+				delayTimeIndex++
+			}
+			lockTime = process.uptime()
+
+			transport.sendMail(xtend({
+				to: loginRequest.contactAddress,
+				html: createHtmlEmail(loginRequest.token)
+			}, defaultMailOptions), callback)
+			emitter.emit('sent email', true)
+		} else {
+			//console.log('fail -> now:'+process.uptime()+', waitUntil:'+(lockTime+delayTimes[delayTimeIndex]))
+			emitter.emit('sent email', false)
+		}
 	})
 }
