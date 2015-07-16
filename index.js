33609d058327df84727c4e3ac27fba8fd8333ca3
Joseph Dykstra <josephdykstra@gmail.com>
2015-07-15 23:00:14 -0500

    remove stray comment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var xtend = require('xtend')
 module.exports = function loginMailer(emitter, createHtmlEmail, transportOptions, defaultMailOptions, callback) {
 	var transport = nodemailer.createTransport(transportOptions)
 
-	emitter.on('authentication initiated', function (loginRequest) { //don't use timers plz. they make tests hang.
+	emitter.on('authentication initiated', function (loginRequest) {
 		transport.sendMail(xtend({
 			to: loginRequest.contactAddress,
 			html: createHtmlEmail(loginRequest.token)
